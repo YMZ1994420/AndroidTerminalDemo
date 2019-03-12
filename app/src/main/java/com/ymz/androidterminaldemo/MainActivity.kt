7560397f033b41abf914354e2b1227d639729e17
YMZ1994 <1227341618@qq.com>
2019-03-12 14:18:34 +0800

    删除示例中多余的代码

--- app/src/main/java/com/ymz/androidterminaldemo/MainActivity.kt
+++ app/src/main/java/com/ymz/androidterminaldemo/MainActivity.kt
@@ -13,9 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Example of a call to a native method
-
         btnTest.setOnClickListener {
             Terminal{
                 handler.post {
@@ -24,20 +21,6 @@ class MainActivity : AppCompatActivity() {
             }.apply {
                 executeCmd("ls")
             }
-        }
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
         }
     }
 }
